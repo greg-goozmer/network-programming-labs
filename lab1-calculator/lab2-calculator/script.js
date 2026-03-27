f0aac0a10ffce41588f7a9989d4adc6d6abb84c8
greg-goozmer <zherebenkoff170306@gmail.com>
2026-03-27 10:52:22 +0300

    Extra panel for additional functions

--- lab1-calculator/lab2-calculator/script.js
+++ lab1-calculator/lab2-calculator/script.js
@@ -1,19 +1,14 @@
 window.onload = function() {
-    const div = document.getElementById('message');
+    const extraToggle = document.getElementById('btn_extra_toggle');
+    const extraPanel = document.getElementById('extraPanel');
 
-    document.getElementById('btn1').onclick = () => {
-        alert(div.innerHTML); // "Привет, <b>мир</b>!"
-    };
-
-    document.getElementById('btn2').onclick = () => {
-        alert(div.textContent); // "Привет, мир!"
-    };
-
-    document.getElementById('btn3').onclick = () => {
-        div.innerHTML = 'Новый <i>текст</i>'; // вставится с курсивом
-    };
-
-    document.getElementById('btn4').onclick = () => {
-        div.textContent = 'Просто текст <i>не будет курсивом</i>'; // теги не сработают
+    extraToggle.onclick = () => {
+        if (extraPanel.style.display === 'none') {
+            extraPanel.style.display = 'block';
+            extraToggle.textContent = ' ♺';
+        } else {
+            extraPanel.style.display = 'none';
+            extraToggle.textContent = ' ♺';
+        }
     };
 }
